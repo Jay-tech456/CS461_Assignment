451e51caf47890acecdd73534b5185a22b07bde4
Manjesh Prasad <59748163+Jay-tech456@users.noreply.github.com>
2021-09-30 14:55:43 -0700

    Updated functions, will still throw errors
    We still need to implement the constraints of the jealous husband problem, and be able to traverse the entire list. Overall it works with H1 W1 being passed in, but fails to do everything else

--- Main.py
+++ Main.py
@@ -4,37 +4,29 @@ from Problem import Problem
 from Node import Node
 
 
-def depth_first_graph_search(problem):
+def breadth_first_tree_search(problem):
     """
     [Figure 3.7]
-    Search the deepest nodes in the search tree first.
+    Search the shallowest nodes in the search tree first.
     Search through the successors of a problem to find a goal.
     The argument frontier should be an empty queue.
-    Does not get trapped by loops.
-    If two paths reach a state, only use the first one.
+    Repeats infinitely in case of loops.
     """
-    frontier = [(Node(problem.initial))]  # Stack
-    print("frontier " + str(frontier))
 
-    explored = set()
+    frontier = deque([Node(problem.initial)])  # FIFO queue
 
     while frontier:
-        node = frontier.pop()
+        node = frontier.popleft()
         if problem.goal_test(node.state):
             return node
-
-        print("Here is the node.state" + str(node.state))
-        explored.add(node.state)
-        print(explored)                 # Should print out the initial string "H1H2W1W2", and then hits the extend
-        frontier.extend(child for child in node.expand(problem) if child.state not in explored and child not in frontier)
+        frontier.extend(node.expand(problem))
     return None
 
 
 
 def main():
-    p = Problem("H1,W1", None)
-    depth_first_graph_search(p)
-    solution = depth_first_graph_search(p)
+    p = Problem("H1,W1,H2,W2", None)
+    solution = breadth_first_tree_search(p)
     if(solution == None):
         print("No PATHS FOUND")
     else:
--- Problem.py
+++ Problem.py
@@ -1,45 +1,3 @@
-#
-# class Problem:
-#     # There is a chance that we might need to pass in the objects as the husband and wives
-#     def __init__(self, initial, goal=None):
-#         # This is the class constructor,
-#         # T
-#         self.initial = initial  # the initial state of the entire program
-#         self.goal = goal  # The goal is to get everyone into the other side of the river
-#
-#     def actions(self, state):
-#         # This will return the actions that can be executred in the given state. The result would be a list
-#         # if there are still more actions.
-#         # This method returns all possible actions agent can execute in the given state state
-#         raise NotImplementedError("The class should implement this ")
-#         return self.state
-#
-#     def result(self, state, action):
-#         # return the state that results from executing the given actions in the given state.... the action must be
-#         # one of self.actions(state)
-#         raise NotImplementedError
-#
-#     def goal_test(self, state):
-#         # returns true if the state is a goal. The default method compares to the state to self.goal
-#         # or checks for state in self.goal if it is a list, as specified in the
-#         # constructor. Overide this mehtod if checking against a single self.goal is not enough.
-#         if isinstance(self.goal, list):
-#             return is_in(state, self.goal)
-#         else:
-#             return state == self.goal
-#
-#     def path_cost(self, c, state1, action, state2):
-#         # returns the cost of a solution path that arrives at state 2 from state 1 through actions
-#         # assuming that there exist a state that cost 1 up to state.
-#         # if the path does matter, it will consider c and maybe state1, but if the path does not matter, it will
-#         # only look at state2
-#
-#         # We are moving the couples from state1 to state 2, and the total mount of actions that is cost initially is
-#         # 0, but once moving between state1 to state2, if the order matters, then we have to increment by one, but if the
-#         # order does not matter, than bypass
-#
-#         return c + 1
-
 class Problem:
     """The abstract class for a formal problem. You should subclass
     this and implement the methods actions and result, and possibly
@@ -58,10 +16,12 @@ class Problem:
         state. The result would typically be a list, but if there are
         many actions, consider yielding them one at a time in an
         iterator, rather than building them all at once."""
+        print("state " + str(state))
 
-        b = state.split(",")
-        print("Here is the state that is being passed in " + str(state))
-        print("Here is B " + str(b))
+        if len(state) > 1:
+            b = state.split(",")
+        else:
+            b = state
         a = []
         i = 0
         while i < len(b):                       # O(n) W1H1
@@ -79,10 +39,12 @@ class Problem:
         action in the given state. The action must be one of
         self.actions(state)."""
         a = action.split(" ")
-        b = state.split(",")
-        print("Hello world ")
-        print(" Here is A " + str(a))
-        print(" Here is B " + str(b))
+        if len(state) > 1:
+            b = state.split(",")
+        elif not state:
+            b = []
+        else:
+            b = state
 
         for count in range(len(a)):
             b.remove(a[count])
